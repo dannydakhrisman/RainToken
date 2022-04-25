e940427db67873c18fffabd68c59d7a452d68d7a
Danny Dakhrisman <36442027+dannydakhrisman@users.noreply.github.com>
2022-04-25 16:00:10 +0700

    Update RainToken.sol

--- RainToken.sol
+++ RainToken.sol
@@ -5,9 +5,9 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract RainToken is ERC20 {
-    constructor() ERC20("RainToken", "RAIN"){
-        _mint(msg.sender, 20000 * 10 ** 18);
+contract RainToken is ERC20 { //Ubah "RainToken" menjadi nama token Anda
+    constructor() ERC20("RainToken", "RAIN"){ //Ubah "RainToken" menjadi nama token Anda dan "RAIN" menjadi simbol token Anda
+        _mint(msg.sender, 20000 * 10 ** 18); //Ubah "200000" menjadi total supply token Anda dan 18 adalah decimal yang digunakan. 
     }
 
 function mint(address to , uint amount) public{
